3345327e3ce6afacddc5c1d89bfa0d0fb4ff4203
Aqsa Shah <aqsashah6347@gmail.com>
2026-07-05 21:49:56 +0500

    liquid glass underprocess

--- TMS Frontend/task-system/src/App.jsx
+++ TMS Frontend/task-system/src/App.jsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
 import Sidebar from "./components/layout/Sidebar";
 import Header from "./components/layout/Header";
+import BackgroundOrbs from "./components/layout/BackgroundOrbs";
 import AppRoutes from "./routes/AppRoutes";
 import { useAuthStore } from "./store/useAuthStore";
 
@@ -10,18 +11,23 @@ export default function App() {
   const isLoginPage = location.pathname === "/login";
 
   if (isLoginPage) {
-    return <AppRoutes />;
+    return (
+      <>
+        <BackgroundOrbs />
+        <AppRoutes />
+      </>
+    );
   }
 
   return (
-    <div className="flex h-screen bg-bg">
+    <div className="relative min-h-screen">
+      <BackgroundOrbs />
       <Sidebar isAdmin={user?.role === "admin"} />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Header userName={user?.name || "Guest"} />
-        <main className="flex-1 overflow-y-auto p-6">
-          <AppRoutes />
-        </main>
-      </div>
+      <Header />
+
+      <main className="relative z-10 pt-24 pl-28 pr-6 pb-6 min-h-screen">
+        <AppRoutes />
+      </main>
     </div>
   );
 }
